11cac18e2e66363bc637620dc186c952b02713eb
Gabriel N <gabonunezm@estudiantec.cr>
2023-04-18 15:44:20 -0600

    Arreglos en el dummy

--- dragueur de mines/src/dragueur/de/mines/DummyController.java
+++ dragueur de mines/src/dragueur/de/mines/DummyController.java
@@ -17,7 +17,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
